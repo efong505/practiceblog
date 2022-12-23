39361f1b2468f486a39d68c5a05fb4871ab8c7a4
efong505 <efong505@nmsu.edu>
2022-12-22 18:31:19 -0700

    sdf

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,14 +1,14 @@
 from django import template
-import markdown
+# import markdown
 from django.db.models import Count
 from . .models import Post
 register = template.Library()
-from django.utils.safestring import mark_safe
+# from django.utils.safestring import mark_safe
 
 
-@register.filter(name='markdown')
-def markdown_format(text):
-    return mark_safe(markdown.markdown(text))
+# @register.filter(name='markdown')
+# def markdown_format(text):
+#     return mark_safe(markdown.markdown(text))
 @register.simple_tag
 def total_posts():
     return Post.published.count()
